ba42f56b8fb8391271dfe583bede5dbf9caa9434
Quentin Garchery <garchery.quentin@gmail.com>
2024-10-04 09:26:38 +0200

    refactor: renaming owner IRM

--- src/IRM.sol
+++ src/IRM.sol
@@ -4,15 +4,15 @@ pragma solidity 0.8.27;
 import {VaultsV2} from "./VaultsV2.sol";
 
 contract IRM {
-    // Note that rateManager may be controlled by the curator, if the curator has the ability to change the IRM.
-    address public immutable rateManager;
+    // Note that owner may be controlled by the curator, if the curator has the ability to change the IRM.
+    address public immutable owner;
     VaultsV2 public immutable vault;
 
     // Notice how this makes it O(1) in the number of markets.
     uint256 public rate;
 
-    constructor(address _rateManager, VaultsV2 _vault) {
-        rateManager = _rateManager;
+    constructor(address _owner, VaultsV2 _vault) {
+        owner = _owner;
         vault = _vault;
     }
 
@@ -22,7 +22,7 @@ contract IRM {
     // Note also that the formula and its coefficients are arbitrary at the moment:
     // it only illustrates that the rate is meant to be controlling totalAssets to target realAssets.
     function setRate() public {
-        require(msg.sender == rateManager);
+        require(msg.sender == owner);
         rate = vault.realRate() + vault.realAssets() / 30 days - vault.totalAssets() / 30 days;
     }
 }
