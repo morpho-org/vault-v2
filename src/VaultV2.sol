6d9619e5842e0fbfdf096b2fc073c2cffc5048cc
MathisGD <mathis@morpho.xyz>
2025-02-25 18:07:03 +0100

    chore: fmt

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.24;
+pragma solidity 0.8.28;
 
 import {ERC20, IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Math} from "../lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
