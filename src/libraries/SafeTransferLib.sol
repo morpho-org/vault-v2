ccb4ae8fb7d5aa295b99e08cebf37fa92676b1ed
MathisGD <mathis@morpho.xyz>
2025-04-08 14:43:04 +0200

    chore: fmt

--- src/libraries/SafeTransferLib.sol
+++ src/libraries/SafeTransferLib.sol
@@ -8,8 +8,7 @@ library SafeTransferLib {
     function safeTransfer(IERC20 token, address to, uint256 value) internal {
         require(address(token).code.length > 0, ErrorsLib.NoCode());
 
-        (bool success, bytes memory returndata) =
-            address(token).call(abi.encodeCall(IERC20.transfer, (to, value)));
+        (bool success, bytes memory returndata) = address(token).call(abi.encodeCall(IERC20.transfer, (to, value)));
         require(success, ErrorsLib.TransferReverted());
         require(returndata.length == 0 || abi.decode(returndata, (bool)), ErrorsLib.TransferReturnedFalse());
     }
