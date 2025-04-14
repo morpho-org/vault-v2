e88e209090da5245f7a43e7f134cf05d8ddcd34f
Adrien Husson <adhusson@gmail.com>
2025-04-14 16:54:08 +0200

    add tests

--- test/ERC20Test.sol
+++ test/ERC20Test.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.28;
 
 import {BaseTest} from "./BaseTest.sol";
+import {stdError} from "forge-std/StdError.sol";
+import "../src/libraries/EventsLib.sol";
+import {ErrorsLib} from "../src/libraries/ErrorsLib.sol";
 
 contract ERC20Test is BaseTest {
     uint256 constant MAX_DEPOSIT = 1e18 ether;
@@ -15,39 +18,192 @@ contract ERC20Test is BaseTest {
 
     function testMint(uint256 amount) public {
         vm.assume(amount <= MAX_DEPOSIT);
+
+        vm.expectEmit();
+        emit EventsLib.Transfer(address(0), address(this), amount);
+
         vault.mint(amount, address(this));
-        assertEq(vault.balanceOf(address(this)), amount);
+        assertEq(vault.totalSupply(), amount, "total supply");
+        assertEq(vault.balanceOf(address(this)), amount, "balance");
     }
 
-    function testBurn(uint256 amount) public {
+    function testMintZeroAddress(uint256 amount) public {
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        vault.mint(amount, address(0));
+    }
+
+    function testBurn(uint256 amount, uint amountRedeemed) public {
         vm.assume(amount <= MAX_DEPOSIT);
+        amountRedeemed = bound(amountRedeemed, 0, amount);
+
         vault.mint(amount, address(this));
-        vault.redeem(amount, address(this), address(this));
-        assertEq(vault.balanceOf(address(this)), 0);
+        vm.expectEmit();
+        emit EventsLib.Transfer(address(this), address(0), amountRedeemed);
+
+        vault.redeem(amountRedeemed, address(this), address(this));
+
+        assertEq(vault.totalSupply(), amount - amountRedeemed, "total supply");
+        assertEq(vault.balanceOf(address(this)), amount - amountRedeemed, "balance");
+    }
+
+    function testBurnZeroAddress() public {
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        vault.redeem(0, address(this), address(0));
     }
 
     function testApprove(address spender, uint256 amount) public {
         vm.assume(amount <= MAX_DEPOSIT);
-        vault.approve(spender, amount);
+        vm.expectEmit();
+        emit EventsLib.Approval(address(this), address(spender), amount);
+
+        assertTrue(vault.approve(spender, amount));
         assertEq(vault.allowance(address(this), spender), amount);
     }
 
-    function testTransfer(address to, uint256 amount) public {
+    function testTransfer(address to, uint256 amount, uint amountTransferred) public {
         vm.assume(amount <= MAX_DEPOSIT);
         vm.assume(to != address(0));
+        amountTransferred = bound(amountTransferred, 0, amount);
+
         vault.mint(amount, address(this));
-        vault.transfer(to, amount);
-        assertEq(vault.balanceOf(to), amount);
+
+        vm.expectEmit();
+        emit EventsLib.Transfer(address(this), address(to), amountTransferred);
+
+        assertTrue(vault.transfer(to, amountTransferred));
+
+        assertEq(vault.totalSupply(), amount, "total supply");
+        if (address(this) == to) {
+            assertEq(vault.balanceOf(address(this)), amount, "balance");
+        } else {
+            assertEq(vault.balanceOf(address(this)), amount - amountTransferred, "balance from");
+            assertEq(vault.balanceOf(to), amountTransferred, "balance to");
+        }
     }
 
-    function testTransferFrom(address to, uint256 amount) public {
+    function testTransferZeroAddress(uint256 amount) public {
+        vault.mint(amount, address(this));
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        vault.transfer(address(0), amount);
+    }
+
+    function testTransferFrom(address from, address to, uint256 amount, uint amountTransferred, uint amountApproved) public {
         vm.assume(amount <= MAX_DEPOSIT);
+        amountApproved = bound(amountApproved, 0, amount);
+        amountTransferred = bound(amountTransferred, 0, amountApproved);
+
+        vm.assume(from != address(0));
+        vm.assume(to != address(0));
+        vault.mint(amount, from);
+
+        vm.prank(from);
+        vault.approve(address(this), amountApproved);
+
+        vm.expectEmit();
+        emit EventsLib.Transfer(from,to, amountTransferred);
+        vault.transferFrom(from, to, amountTransferred);
+
+        assertEq(vault.allowance(from, address(this)), amountApproved - amountTransferred, "allowance");
+        if (from == to) {
+            assertEq(vault.balanceOf(from), amount, "balance");
+        } else {
+            assertEq(vault.balanceOf(from), amount - amountTransferred, "balance from");
+            assertEq(vault.balanceOf(to), amountTransferred, "balance to");
+        }
+    }
+
+    function testTransferFromSenderZeroAddress(address to, uint256 amount) public {
+        vm.assume(to != address(0));
+        vm.startPrank(address(0));
+        vault.approve(address(this),type(uint).max);
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        vault.transferFrom(address(0), to, amount);
+        vm.stopPrank();
+    }
+
+    function testTransferFromReceiverZeroAddress(address from, uint256 amount) public {
+        vm.assume(from != address(0));
+        vault.mint(amount, from);
+        vm.prank(from);
+        vault.approve(address(this), type(uint).max);
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        vault.transferFrom(from, address(0), amount);
+    }
+
+    function testInfiniteApproveTransferFrom(address from, address to, uint256 amount, uint amountTransferred) public {
+        vm.assume(amount <= MAX_DEPOSIT);
+        amountTransferred = bound(amountTransferred, 0, amount);
+
+        vm.assume(from != address(0));
+        vm.assume(to != address(0));
+        vault.mint(amount, from);
+
+        vm.prank(from);
+        vault.approve(address(this), type(uint256).max);
+
+        vm.expectEmit();
+        emit EventsLib.Transfer(from, to, amountTransferred);
+
+        vault.transferFrom(from, to, amountTransferred);
+        assertEq(vault.allowance(from, address(this)), type(uint).max, "allowance");
+        if (from == to) {
+            assertEq(vault.balanceOf(from), amount, "balance");
+        } else {
+            assertEq(vault.balanceOf(from), amount - amountTransferred, "balance from");
+            assertEq(vault.balanceOf(to), amountTransferred, "balance to");
+        }
+    }
+
+    function testMintOverMaxUintReverts() public {
+        vault.mint(type(uint256).max, address(this));
+        vm.expectRevert(stdError.arithmeticError);
+        vault.mint(1, address(this));
+    }
+
+    function testTransferInsufficientBalanceReverts(address to, uint amount) public {
+        amount = bound(amount, 0, type(uint).max-1);
         vm.assume(to != address(0));
         vault.mint(amount, address(this));
-        assertEq(vault.balanceOf(address(this)), amount);
-        vault.approve(to, amount);
-        vm.prank(to);
-        vault.transferFrom(address(this), to, amount);
-        assertEq(vault.balanceOf(to), amount);
+        vm.expectRevert(stdError.arithmeticError);
+        vault.transfer(to, amount + 1);
+    }
+
+    function testTransferFromInsufficientAllowanceReverts(address from, address to, uint allowance) public {
+        vm.assume(from != address(0));
+        vm.assume(to != address(0));
+
+        allowance = bound(allowance, 0, type(uint).max - 1);
+        vault.mint(allowance, from);
+
+        vm.prank(from);
+        vault.approve(address(this), allowance);
+
+        vm.expectRevert(stdError.arithmeticError);
+        vault.transferFrom(from, to, allowance + 1);
+    }
+
+    function testTransferFromInsufficientBalanceReverts(address from, address to, uint allowance) public {
+        vm.assume(from != address(0));
+        vm.assume(to != address(0));
+        allowance = bound(allowance, 1, type(uint).max);
+        vault.mint(allowance - 1, from);
+
+        vm.prank(from);
+        vault.approve(address(this), allowance);
+
+        vm.expectRevert(stdError.arithmeticError);
+        vault.transferFrom(from, to, allowance);
+    }
+
+    function testBurnInsufficientBalanceReverts(address to, uint256 mintAmount, uint256 burnAmount)
+        public
+    {
+        vm.assume(to != address(0));
+        mintAmount = bound(mintAmount, 0, type(uint).max - 1);
+        burnAmount = _bound(burnAmount, mintAmount + 1, type(uint256).max);
+
+        vault.mint(mintAmount, to);
+        vm.expectRevert(stdError.arithmeticError);
+        vault.redeem(burnAmount, to, to);
     }
 }
