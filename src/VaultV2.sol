8f4f02513a2083891da94535cb23a5cedf3a3ae7
MathisGD <mathis@morpho.xyz>
2025-04-23 12:30:11 +0200

    refactor: only one market

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -61,10 +61,8 @@ contract VaultV2 is IVaultV2 {
     /// @dev By design, double counting some stuff.
     mapping(bytes32 => uint256) public allocation;
 
-    address public depositAdapter;
-    bytes public depositData;
-    address public withdrawAdapter;
-    bytes public withdrawData;
+    address public liquidityAdapter;
+    bytes public liquidityData;
 
     /// @dev calldata => executable at
     mapping(bytes => uint256) public validAt;
@@ -199,10 +197,8 @@ contract VaultV2 is IVaultV2 {
 
     // Note how the discrepancy between transferred amount and increase in market.totalAssets() is handled:
     // it is not reflected in vault.totalAssets() but will have an impact on interest.
-    function reallocateFromIdle(address adapter, bytes memory data, uint256 amount) external {
-        require(
-            isAllocator[msg.sender] || isSentinel[msg.sender] || msg.sender == address(this), ErrorsLib.NotAllocator()
-        );
+    function reallocateFromIdle(address adapter, bytes memory data, uint256 amount) public {
+        require(isAllocator[msg.sender] || isSentinel[msg.sender], ErrorsLib.NotAllocator());
         require(isAdapter[adapter], ErrorsLib.NotAdapter());
 
         SafeTransferLib.safeTransfer(IERC20(asset), adapter, amount);
@@ -220,10 +216,8 @@ contract VaultV2 is IVaultV2 {
 
     // Note how the discrepancy between transferred amount and decrease in market.totalAssets() is handled:
     // it is not reflected in vault.totalAssets() but will have an impact on interest.
-    function reallocateToIdle(address adapter, bytes memory data, uint256 amount) external {
-        require(
-            isAllocator[msg.sender] || isSentinel[msg.sender] || msg.sender == address(this), ErrorsLib.NotAllocator()
-        );
+    function reallocateToIdle(address adapter, bytes memory data, uint256 amount) public {
+        require(isAllocator[msg.sender] || isSentinel[msg.sender], ErrorsLib.NotAllocator());
         require(isAdapter[adapter], ErrorsLib.NotAdapter());
 
         bytes32[] memory ids = IAdapter(adapter).allocateOut(data, amount);
@@ -235,14 +229,9 @@ contract VaultV2 is IVaultV2 {
         SafeTransferLib.safeTransferFrom(IERC20(asset), adapter, address(this), amount);
     }
 
-    function setDepositData(address newDepositAdapter, bytes memory newDepositData) external timelocked {
-        depositAdapter = newDepositAdapter;
-        depositData = newDepositData;
-    }
-
-    function setWithdrawData(address newWithdrawAdapter, bytes memory newWithdrawData) external timelocked {
-        withdrawAdapter = newWithdrawAdapter;
-        withdrawData = newWithdrawData;
+    function setLiquidityMarket(address newLiquidityAdapter, bytes memory newLiquidityData) external timelocked {
+        liquidityAdapter = newLiquidityAdapter;
+        liquidityData = newLiquidityData;
     }
 
     /* EXCHANGE RATE */
@@ -329,7 +318,7 @@ contract VaultV2 is IVaultV2 {
         _mint(receiver, shares);
         totalAssets += assets;
 
-        try this.reallocateFromIdle(depositAdapter, depositData, assets) {} catch {}
+        try this.reallocateFromIdle(liquidityAdapter, liquidityData, assets) {} catch {}
     }
 
     // TODO: how to hook on deposit so that assets are atomically allocated ?
@@ -348,8 +337,8 @@ contract VaultV2 is IVaultV2 {
 
     function _withdraw(uint256 assets, uint256 shares, address receiver, address supplier) internal {
         uint256 idleAssets = IERC20(asset).balanceOf(address(this));
-        if (assets > idleAssets) {
-            try this.reallocateToIdle(withdrawAdapter, withdrawData, assets - idleAssets) {} catch {}
+        if (assets > idleAssets && liquidityAdapter != address(0)) {
+            reallocateToIdle(liquidityAdapter, liquidityData, assets - idleAssets);
         }
         uint256 _allowance = allowance[supplier][msg.sender];
         if (msg.sender != supplier && _allowance != type(uint256).max) {
@@ -429,8 +418,7 @@ contract VaultV2 is IVaultV2 {
         if (selector == IVaultV2.increaseRelativeCap.selector) return sender == curator;
         if (selector == IVaultV2.decreaseRelativeCap.selector) return sender == curator;
         // Allocator actions.
-        if (selector == IVaultV2.setDepositData.selector) return isAllocator[sender];
-        if (selector == IVaultV2.setWithdrawData.selector) return isAllocator[sender];
+        if (selector == IVaultV2.setLiquidityMarket.selector) return isAllocator[sender];
         return false;
     }
 
