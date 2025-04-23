a83834612e14aa0b102a2018a01f0f1eb7415968
MathisGD <74971347+MathisGD@users.noreply.github.com>
2025-04-23 18:48:42 +0200

    Update test/LiquidityMarketTest.sol
    Co-authored-by: Adrien Husson <adhusson@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- test/LiquidityMarketTest.sol
+++ test/LiquidityMarketTest.sol
@@ -3,14 +3,26 @@ pragma solidity 0.8.28;
 
 import "./BaseTest.sol";
 
+contract RecordingAdapter {
+    bytes public recordedData;
+    uint256 public recordedAmount;
+
+    function allocateIn(bytes memory data, uint256 amount) external returns (bytes32[] memory ids) {
+        recordedData = data;
+        recordedAmount = amount;
+        ids = new bytes32[](0);
+    }
+}
+
 contract LiquidityMarketTest is BaseTest {
     using MathLib for uint256;
 
-    bytes recodedData;
-    uint256 recodedAmount;
+    RecordingAdapter public adapter;
 
     function setUp() public override {
         super.setUp();
+
+        adapter = new RecordingAdapter();
 
         deal(address(underlyingToken), address(this), type(uint256).max);
         underlyingToken.approve(address(vault), type(uint256).max);
@@ -18,39 +30,33 @@ contract LiquidityMarketTest is BaseTest {
 
     function testLiquidityMarketDeposit(bytes memory data, uint256 assets) public {
         vm.prank(owner);
-        vault.submit(abi.encodeWithSelector(IVaultV2.setIsAdapter.selector, address(this), true));
-        vault.setIsAdapter(address(this), true);
+        vault.submit(abi.encodeWithSelector(IVaultV2.setIsAdapter.selector, address(adapter), true));
+        vault.setIsAdapter(address(adapter), true);
 
         vm.prank(allocator);
-        vault.submit(abi.encodeWithSelector(IVaultV2.setLiquidityMarket.selector, address(this), data));
-        vault.setLiquidityMarket(address(this), data);
+        vault.submit(abi.encodeWithSelector(IVaultV2.setLiquidityMarket.selector, address(adapter), data));
+        vault.setLiquidityMarket(address(adapter), data);
 
         vault.deposit(assets, address(this));
 
-        assertEq(recodedData, data);
-        assertEq(recodedAmount, assets);
+        assertEq(adapter.recordedData(), data);
+        assertEq(adapter.recordedAmount(), assets);
     }
 
     function testLiquidityMarketMint(bytes memory data, uint256 shares) public {
         vm.prank(owner);
-        vault.submit(abi.encodeWithSelector(IVaultV2.setIsAdapter.selector, address(this), true));
-        vault.setIsAdapter(address(this), true);
+        vault.submit(abi.encodeWithSelector(IVaultV2.setIsAdapter.selector, address(adapter), true));
+        vault.setIsAdapter(address(adapter), true);
 
         vm.prank(allocator);
-        vault.submit(abi.encodeWithSelector(IVaultV2.setLiquidityMarket.selector, address(this), data));
-        vault.setLiquidityMarket(address(this), data);
+        vault.submit(abi.encodeWithSelector(IVaultV2.setLiquidityMarket.selector, address(adapter), data));
+        vault.setLiquidityMarket(address(adapter), data);
 
         uint256 assets = shares.mulDivDown(vault.totalAssets() + 1, vault.totalSupply() + 1);
 
         vault.mint(shares, address(this));
 
-        assertEq(recodedData, data);
-        assertEq(recodedAmount, assets);
-    }
-
-    function allocateIn(bytes memory data, uint256 amount) external returns (bytes32[] memory ids) {
-        recodedData = data;
-        recodedAmount = amount;
-        ids = new bytes32[](0);
+        assertEq(adapter.recordedData(), data);
+        assertEq(adapter.recordedAmount(), assets);
     }
 }
