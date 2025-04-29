5578a2ca3bc7b61d16f0224f2046070bf1cf6955
MathisGD <74971347+MathisGD@users.noreply.github.com>
2025-04-29 15:35:23 +0200

    Update src/libraries/periphery/VaultV2AddressLib.sol
    Co-authored-by: Quentin Garchery <garchery.quentin@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/libraries/periphery/VaultV2AddressLib.sol
+++ src/libraries/periphery/VaultV2AddressLib.sol
@@ -10,6 +10,6 @@ library VaultV2AddressLib {
         returns (address)
     {
         bytes32 initCodeHash = keccak256(abi.encodePacked(type(VaultV2).creationCode, abi.encode(owner, asset)));
-        return address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), address(factory), salt, initCodeHash)))));
+        return address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), factory, salt, initCodeHash)))));
     }
 }
