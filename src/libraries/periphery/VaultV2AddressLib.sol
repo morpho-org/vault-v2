710f1ab37a5279f2767a559238fc42b82bdf7993
MathisGD <mathis@morpho.xyz>
2025-04-29 15:16:48 +0200

    chore:fmt

--- src/libraries/periphery/VaultV2AddressLib.sol
+++ src/libraries/periphery/VaultV2AddressLib.sol
@@ -4,17 +4,12 @@ pragma solidity ^0.8.0;
 import {VaultV2} from "../../VaultV2.sol";
 
 library VaultV2AddressLib {
-    function computeVaultV2Address(
-        address factory,
-        address owner,
-        address asset,
-        bytes32 salt
-    ) internal pure returns (address) {
-        bytes32 initCodeHash = keccak256(
-            abi.encodePacked(
-                type(VaultV2).creationCode, abi.encode(owner, asset)
-            )
-        );
+    function computeVaultV2Address(address factory, address owner, address asset, bytes32 salt)
+        internal
+        pure
+        returns (address)
+    {
+        bytes32 initCodeHash = keccak256(abi.encodePacked(type(VaultV2).creationCode, abi.encode(owner, asset)));
         return address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), address(factory), salt, initCodeHash)))));
     }
 }
--- test/FactoryTest.sol
+++ test/FactoryTest.sol
@@ -6,7 +6,8 @@ import {VaultV2AddressLib} from "../src/libraries/periphery/VaultV2AddressLib.so
 
 contract FactoryTest is BaseTest {
     function testCreateVaultV2(address _owner, address _asset, bytes32 _salt) public {
-        address expectedVaultAddress = VaultV2AddressLib.computeVaultV2Address(address(vaultFactory), _owner, _asset, _salt);
+        address expectedVaultAddress =
+            VaultV2AddressLib.computeVaultV2Address(address(vaultFactory), _owner, _asset, _salt);
         vm.expectEmit();
         emit EventsLib.CreateVaultV2(expectedVaultAddress, _owner, _asset);
         IVaultV2 vault = IVaultV2(vaultFactory.createVaultV2(_owner, _asset, _salt));
