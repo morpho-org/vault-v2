0d9ae3a0042c78d1037f889049673dde0c02f4df
MerlinEgalite <egalite.merlin@gmail.com>
2025-05-02 14:41:54 +0200

    refactor: revert move lastupdate

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -312,11 +312,11 @@ contract VaultV2 is IVaultV2 {
         (uint256 newTotalAssets, uint256 performanceFeeShares, uint256 managementFeeShares) = accrueInterestView();
 
         totalAssets = newTotalAssets;
-        lastUpdate = block.timestamp;
 
         if (performanceFeeShares != 0) createShares(performanceFeeRecipient, performanceFeeShares);
         if (managementFeeShares != 0) createShares(managementFeeRecipient, managementFeeShares);
 
+        lastUpdate = block.timestamp;
         emit EventsLib.AccrueInterest(newTotalAssets, performanceFeeShares, managementFeeShares);
     }
 
