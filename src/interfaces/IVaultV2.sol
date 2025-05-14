38d724a14d45d83d222f4b793e9865beb85a9eb1
MathisGD <mathis@morpho.xyz>
2025-05-14 15:18:08 +0200

    docs: minor change

--- src/interfaces/IVaultV2.sol
+++ src/interfaces/IVaultV2.sol
@@ -16,13 +16,13 @@ interface IVaultV2 is IERC20 {
     // ERC-4626-v2
     function asset() external view returns (address);
     function totalAssets() external view returns (uint256);
-    function previewDeposit(uint256 assets) external view returns (uint256);
+    function previewDeposit(uint256 assets) external view returns (uint256 shares);
     function deposit(uint256 assets, address receiver) external returns (uint256 shares);
-    function previewMint(uint256 shares) external view returns (uint256);
+    function previewMint(uint256 shares) external view returns (uint256 assets);
     function mint(uint256 shares, address receiver) external returns (uint256 assets);
-    function previewWithdraw(uint256 assets) external view returns (uint256);
+    function previewWithdraw(uint256 assets) external view returns (uint256 shares);
     function withdraw(uint256 assets, address receiver, address owner) external returns (uint256 shares);
-    function previewRedeem(uint256 shares) external view returns (uint256);
+    function previewRedeem(uint256 shares) external view returns (uint256 assets);
     function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets);
 
     // State variables
