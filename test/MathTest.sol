1f08b710e7d54dbc0f2af63529240fe520b5f7a1
Adrien Husson <adhusson@gmail.com>
2025-05-19 09:41:21 +0200

    remove na test

--- test/MathTest.sol
+++ test/MathTest.sol
@@ -26,10 +26,6 @@ contract MathTest is Test {
         assertEq(MathLib.min(x, y), x < y ? x : y);
     }
 
-    function testZeroFloorSub(uint256 x, uint256 y) public pure {
-        assertEq(MathLib.zeroFloorSub(x, y), x < y ? 0 : x - y);
-    }
-
     function testZeroFloorAddInt(uint256 x, int256 y) public {
         if (y < 0) {
             assertEq(MathLib.zeroFloorAddInt(x, y), x < abs(y) ? 0 : x - abs(y), "down");
