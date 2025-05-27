96329e0668d22b72878c815a0d5ea29fd72ec7e8
MathisGD <mathis@morpho.xyz>
2025-05-27 16:45:30 +0200

    docs: improve docs

--- src/libraries/UtilsLib.sol
+++ src/libraries/UtilsLib.sol
@@ -5,8 +5,7 @@ import {ErrorsLib} from "./ErrorsLib.sol";
 
 library UtilsLib {
     /// @dev Returns the first word returned by a successful static call.
-    /// @dev Returns 0 if no data was returned.
-    /// @dev Returns 0 if the static call reverted.
+    /// @dev Returns 0 if no data was returned or if the static call reverted.
     /// @dev Unlike a low-level solidity call, does not copy all the return data to memory.
     function controlledStaticCall(address to, bytes memory data) internal view returns (bytes32) {
         bytes32[1] memory output;
