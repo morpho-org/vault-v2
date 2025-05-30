ae05f8307082772e5f3b1c45b4b10bb62d4db9ca
Adrien Husson <adhusson@gmail.com>
2025-05-30 13:50:37 +0200

    cache chain id

---
+++ test/GettersTest.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import "./BaseTest.sol";
+
+contract GettersTest is BaseTest {
+    function setUp() public override {
+        super.setUp();
+
+        deal(address(underlyingToken), address(this), type(uint256).max);
+        underlyingToken.approve(address(vault), type(uint256).max);
+    }
+
+    function testDomainSeparatorOtherChainId(uint64 chainId) public {
+        vm.assume(chainId != block.chainid);
+        vm.chainId(chainId);
+        assertEq(vault.DOMAIN_SEPARATOR(), computeDomainSeparator(DOMAIN_TYPEHASH, block.chainid, address(vault)));
+    }
+
+    function testDomainSeparatorSameChainId() public view {
+        assertEq(vault.DOMAIN_SEPARATOR(), computeDomainSeparator(DOMAIN_TYPEHASH, block.chainid, address(vault)));
+    }
+
+    function computeDomainSeparator(bytes32 domainTypehash, uint256 chainId, address account)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encode(domainTypehash, chainId, account));
+    }
+}
