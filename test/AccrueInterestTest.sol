19d28d3dfdd907c876fa25358c819830e3975a98
Adrien Husson <adhusson@gmail.com>
2025-06-01 21:47:24 +0200

    move accrue interest gas test

--- test/AccrueInterestTest.sol
+++ test/AccrueInterestTest.sol
@@ -3,6 +3,54 @@ pragma solidity ^0.8.0;
 
 import "./BaseTest.sol";
 
+contract Reverting {}
+
+// Burn almost all gas and return toReturn.
+contract BurnsAllGas {
+    uint256 immutable returnValue;
+
+    constructor(uint256 _returnValue) {
+        returnValue = _returnValue;
+    }
+
+    fallback() external {
+        // gasToMemoryExpansion uses 1021 gas. Update if needed.
+        // Reserving 50 gas for the return subtraction and the return.
+        uint256 burnAmount = gasleft() - 1021 - 50;
+        uint256 words = gasToMemoryExpansion(burnAmount);
+        uint256 _returnValue = returnValue;
+
+        assembly ("memory-safe") {
+            mstore(mul(sub(words, 1), 32), 1)
+            mstore(0, _returnValue)
+            return(0, 32)
+        }
+    }
+}
+
+// Burn an approximate amount of gas and return 1.
+contract BurnsGas {
+    uint256 immutable burnAmount;
+    BurnsAllGas immutable burner;
+
+    constructor(uint256 _burnAmount) {
+        burnAmount = _burnAmount;
+        burner = new BurnsAllGas(0);
+    }
+
+    fallback() external {
+        (bool s, bytes memory r) = address(burner).call{gas: 64 * burnAmount / 63}("");
+        // No-op to silence warning.
+        s;
+        r;
+        // return 1
+        assembly ("memory-safe") {
+            mstore(0, 1)
+            return(0, 32)
+        }
+    }
+}
+
 contract AccrueInterestTest is BaseTest {
     using MathLib for uint256;
 
@@ -231,6 +279,96 @@ contract AccrueInterestTest is BaseTest {
 
         assertEq(vault.balanceOf(managementFeeRecipient), expectedShares);
     }
+
+    uint256 constant GAS_BURNED_BY_GATE = 30_000;
+    uint256 constant SAFE_GAS_AMOUNT = 8_000_000;
+
+    function testGasRequiredToAccrueIfVicBurnsAllGas() public {
+        // Vault setup
+        deal(address(underlyingToken), address(this), 1e18);
+        underlyingToken.approve(address(vault), type(uint256).max);
+
+        vm.startPrank(curator);
+        // vault.setCurator(address(this));
+
+        uint256 amount = 1e18;
+        uint256 interestPerSecond = amount * MAX_RATE_PER_SECOND / WAD;
+
+        // make accrueInterest as costly as possible
+        // but keep gates cost reasonable since the gate can be changed without accruing interest
+        // rationale is that it is OK for a shares gate to lock users anyway
+        address gate = address(new BurnsGas(GAS_BURNED_BY_GATE));
+        vault.submit(abi.encodeCall(vault.setSharesGate, (gate)));
+        vault.setSharesGate(gate);
+
+        performanceFeeRecipient = makeAddr("performance fee recipient");
+        vault.submit(abi.encodeCall(vault.setPerformanceFeeRecipient, (performanceFeeRecipient)));
+        vault.setPerformanceFeeRecipient(performanceFeeRecipient);
+
+        managementFeeRecipient = makeAddr("management fee recipient");
+        vault.submit(abi.encodeCall(vault.setManagementFeeRecipient, (managementFeeRecipient)));
+        vault.setManagementFeeRecipient(managementFeeRecipient);
+
+        vault.submit(abi.encodeCall(vault.setPerformanceFee, (MAX_PERFORMANCE_FEE)));
+        vault.setPerformanceFee(MAX_PERFORMANCE_FEE);
+
+        vault.submit(abi.encodeCall(vault.setManagementFee, (MAX_MANAGEMENT_FEE)));
+        vault.setManagementFee(MAX_MANAGEMENT_FEE);
+
+        vm.stopPrank();
+
+        vault.deposit(amount, address(this));
+
+        BurnsAllGas burnsAllGas = new BurnsAllGas(interestPerSecond);
+        vm.prank(curator);
+        vault.submit(abi.encodeCall(vault.setVic, (address(burnsAllGas))));
+        vault.setVic(address(burnsAllGas));
+
+        skip(2 weeks);
+
+        // check that vic can still be changed
+        vm.prank(curator);
+        vault.submit(abi.encodeCall(vault.setVic, (address(0))));
+        vault.setVic{gas: SAFE_GAS_AMOUNT}(address(0));
+
+        // check that gas was almost entirely burned
+        assertGt(vm.lastCallGas().gasTotalUsed, SAFE_GAS_AMOUNT * 63 / 64);
+    }
 }
 
-contract Reverting {}
+/* FREE UTILITY FUNCTIONS */
+
+// approximate the inverse of the memory expansion cost function
+// cost function is g(w) = 3w + ⌊w²/512⌋.
+// approximated to w(g) = [ sqrt(1536² + 4 * 512 * g) - 1536 ] / 2.
+function gasToMemoryExpansion(uint256 gas) pure returns (uint256) {
+    return (sqrt(1536 * 1536 + 4 * 512 * gas) - 1536) / 2;
+}
+
+// From
+// https://github.com/Vectorized/solady/blob/b609a9c79ce541c2beca7a7d247665e7c93942a3/src/utils/FixedPointMathLib.sol
+// Stripped comments
+/// @dev Returns the square root of `x`, rounded down.
+function sqrt(uint256 x) pure returns (uint256 z) {
+    assembly ("memory-safe") {
+        z := 181
+
+        let r := shl(7, lt(0xffffffffffffffffffffffffffffffffff, x))
+        r := or(r, shl(6, lt(0xffffffffffffffffff, shr(r, x))))
+        r := or(r, shl(5, lt(0xffffffffff, shr(r, x))))
+        r := or(r, shl(4, lt(0xffffff, shr(r, x))))
+        z := shl(shr(1, r), z)
+
+        z := shr(18, mul(z, add(shr(r, x), 65536)))
+
+        z := shr(1, add(z, div(x, z)))
+        z := shr(1, add(z, div(x, z)))
+        z := shr(1, add(z, div(x, z)))
+        z := shr(1, add(z, div(x, z)))
+        z := shr(1, add(z, div(x, z)))
+        z := shr(1, add(z, div(x, z)))
+        z := shr(1, add(z, div(x, z)))
+
+        z := sub(z, lt(div(x, z), z))
+    }
+}
--- test/ControlledStaticCallTest.sol
+++ test/ControlledStaticCallTest.sol
@@ -45,52 +45,6 @@ contract ReturnsBomb {
     }
 }
 
-// Burn almost all gas and return toReturn.
-contract BurnsAllGas {
-    uint256 immutable returnValue;
-
-    constructor(uint256 _returnValue) {
-        returnValue = _returnValue;
-    }
-
-    fallback() external {
-        // gasToMemoryExpansion uses 1021 gas. Update if needed.
-        // Reserving 50 gas for the return subtraction and the return.
-        uint256 burnAmount = gasleft() - 1021 - 50;
-        uint256 words = gasToMemoryExpansion(burnAmount);
-        uint256 _returnValue = returnValue;
-
-        assembly ("memory-safe") {
-            mstore(mul(sub(words, 1), 32), 1)
-            mstore(0, _returnValue)
-            return(0, 32)
-        }
-    }
-}
-
-// Burn an approximate amount of gas and return 1.
-contract BurnsGas {
-    uint256 immutable burnAmount;
-    BurnsAllGas immutable burner;
-
-    constructor(uint256 _burnAmount) {
-        burnAmount = _burnAmount;
-        burner = new BurnsAllGas(0);
-    }
-
-    fallback() external {
-        (bool s, bytes memory r) = address(burner).call{gas: 64 * burnAmount / 63}("");
-        // No-op to silence warning.
-        s;
-        r;
-        // return 1
-        assembly ("memory-safe") {
-            mstore(0, 1)
-            return(0, 32)
-        }
-    }
-}
-
 contract ControlledStaticCallTest is Test {
     function testSuccess(bytes calldata data) public {
         address account = address(new ReturnsInput());
@@ -141,57 +95,6 @@ contract ControlledStaticCallTest is Test {
         this._testReturnsBombLowLevelStaticCall{gas: gas}(account);
     }
 
-    uint256 constant GAS_BURNED_BY_GATE = 30_000;
-    uint256 constant SAFE_GAS_AMOUNT = 8_000_000;
-
-    function testCanUpdateVicIfVicBurnsAllGas() public {
-        // Vault setup
-        ERC20Mock asset = new ERC20Mock();
-        VaultV2 vault = new VaultV2(address(this), address(asset));
-        deal(address(asset), address(this), 1e18);
-        asset.approve(address(vault), type(uint256).max);
-
-        vault.setCurator(address(this));
-
-        uint256 amount = 1e18;
-        uint256 interestPerSecond = amount * MAX_RATE_PER_SECOND / WAD;
-
-        // make accrueInterest as costly as possible
-        // but keep gates cost reasonable since the gate can be changed without accruing interest
-        // rationale is that it is OK for a shares gate to lock users anyway
-        address gate = address(new BurnsGas(GAS_BURNED_BY_GATE));
-        vault.submit(abi.encodeCall(vault.setSharesGate, (gate)));
-        vault.setSharesGate(gate);
-
-        address performanceFeeRecipient = makeAddr("performance fee recipient");
-        vault.submit(abi.encodeCall(vault.setPerformanceFeeRecipient, (performanceFeeRecipient)));
-        vault.setPerformanceFeeRecipient(performanceFeeRecipient);
-
-        address managementFeeRecipient = makeAddr("management fee recipient");
-        vault.submit(abi.encodeCall(vault.setManagementFeeRecipient, (managementFeeRecipient)));
-        vault.setManagementFeeRecipient(managementFeeRecipient);
-
-        vault.submit(abi.encodeCall(vault.setPerformanceFee, (MAX_PERFORMANCE_FEE)));
-        vault.setPerformanceFee(MAX_PERFORMANCE_FEE);
-
-        vault.submit(abi.encodeCall(vault.setManagementFee, (MAX_MANAGEMENT_FEE)));
-        vault.setManagementFee(MAX_MANAGEMENT_FEE);
-
-        vault.deposit(amount, address(this));
-
-        BurnsAllGas burnsAllGas = new BurnsAllGas(interestPerSecond);
-        vault.submit(abi.encodeCall(vault.setVic, (address(burnsAllGas))));
-        vault.setVic(address(burnsAllGas));
-
-        skip(2 weeks);
-        // check that vic can still be changed
-        vault.submit(abi.encodeCall(vault.setVic, (address(0))));
-        vault.setVic{gas: SAFE_GAS_AMOUNT}(address(0));
-
-        // check that gas was almost entirely burned
-        assertGt(vm.lastCallGas().gasTotalUsed, SAFE_GAS_AMOUNT * 63 / 64);
-    }
-
     /* INTERNAL */
 
     function _testReturnsBomb(address account) external view {
@@ -201,42 +104,5 @@ contract ControlledStaticCallTest is Test {
     function _testReturnsBombLowLevelStaticCall(address account) external view {
         (bool success,) = account.staticcall(hex"");
         success; // No-op to silence warning.
-    }
-}
-
-/* FREE UTILITY FUNCTIONS */
-
-// approximate the inverse of the memory expansion cost function
-// cost function is g(w) = 3w + ⌊w²/512⌋.
-// approximated to w(g) = [ sqrt(1536² + 4 * 512 * g) - 1536 ] / 2.
-function gasToMemoryExpansion(uint256 gas) pure returns (uint256) {
-    return (sqrt(1536 * 1536 + 4 * 512 * gas) - 1536) / 2;
-}
-
-// From
-// https://github.com/Vectorized/solady/blob/b609a9c79ce541c2beca7a7d247665e7c93942a3/src/utils/FixedPointMathLib.sol
-// Stripped comments
-/// @dev Returns the square root of `x`, rounded down.
-function sqrt(uint256 x) pure returns (uint256 z) {
-    assembly ("memory-safe") {
-        z := 181
-
-        let r := shl(7, lt(0xffffffffffffffffffffffffffffffffff, x))
-        r := or(r, shl(6, lt(0xffffffffffffffffff, shr(r, x))))
-        r := or(r, shl(5, lt(0xffffffffff, shr(r, x))))
-        r := or(r, shl(4, lt(0xffffff, shr(r, x))))
-        z := shl(shr(1, r), z)
-
-        z := shr(18, mul(z, add(shr(r, x), 65536)))
-
-        z := shr(1, add(z, div(x, z)))
-        z := shr(1, add(z, div(x, z)))
-        z := shr(1, add(z, div(x, z)))
-        z := shr(1, add(z, div(x, z)))
-        z := shr(1, add(z, div(x, z)))
-        z := shr(1, add(z, div(x, z)))
-        z := shr(1, add(z, div(x, z)))
-
-        z := sub(z, lt(div(x, z), z))
     }
 }
