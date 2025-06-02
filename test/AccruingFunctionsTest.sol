a0f4a17075e8157db8e81baef3995459b3d8ba0e
MathisGD <mathis@morpho.xyz>
2025-06-02 18:42:12 +0200

    chore: fmt

--- test/AccruingFunctionsTest.sol
+++ test/AccruingFunctionsTest.sol
@@ -13,6 +13,8 @@ contract EmptyAdapter is IAdapter {
     function deallocate(bytes memory, uint256) external view returns (bytes32[] memory, uint256) {
         return (ids, 0);
     }
+
+    function realizeLoss(bytes memory) external view returns (bytes32[] memory, uint256) {}
 }
 
 contract AccrueInterestTest is BaseTest {
