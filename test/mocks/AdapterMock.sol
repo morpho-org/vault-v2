7d0870a87f4cbdcaa7c809c0452171e67fa7a6ca
MathisGD <mathis@morpho.xyz>
2025-06-04 00:24:00 +0200

    chore: fix compilation

--- test/mocks/AdapterMock.sol
+++ test/mocks/AdapterMock.sol
@@ -31,6 +31,10 @@ contract AdapterMock is IAdapter {
         return (ids(), 0);
     }
 
+    function realizeLoss(bytes memory data) external returns (bytes32[] memory, uint256) {
+        return (ids(), 0);
+    }
+
     function ids() internal pure returns (bytes32[] memory) {
         bytes32[] memory _ids = new bytes32[](2);
         _ids[0] = keccak256("id-0");
