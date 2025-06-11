2ea783316f2628b2f5de32005b33eadf88263744
Quentin Garchery <garchery.quentin@gmail.com>
2025-06-11 17:50:29 +0200

    docs: remove outdated comment

--- certora/helpers/VaultV2Harness.sol
+++ certora/helpers/VaultV2Harness.sol
@@ -6,7 +6,6 @@ import "../../src/VaultV2.sol";
 contract VaultV2Harness is VaultV2 {
     constructor(address owner, address asset) VaultV2(owner, asset) {}
 
-    // Remove timelocked modifier as this is not checked for revert reasons.in setVicCannotRevertIfDataIsTimelocked.
     function setVicMocked(address newVic) external {
         if (vic.code.length != 0) try this.accrueInterest() {} catch {}
         lastUpdate = uint64(block.timestamp);
