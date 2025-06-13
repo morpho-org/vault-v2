c876f7424c059fce572684c15962ac5414732a70
MathisGD <mathis@morpho.xyz>
2025-06-13 20:28:43 +0200

    chore: copyright

--- test/BaseTest.sol
+++ test/BaseTest.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (c) 2025 Morpho Association
 pragma solidity ^0.8.0;
 
 import {IVaultV2Factory} from "../src/interfaces/IVaultV2Factory.sol";
