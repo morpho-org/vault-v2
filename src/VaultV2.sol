44de10ff09427bed3726ecbe79b62309833154de
Quentin Garchery <garchery.quentin@gmail.com>
2025-07-10 11:01:41 +0200

    feat: add return values to realizeLoss

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -710,7 +710,8 @@ contract VaultV2 is IVaultV2 {
         return shares;
     }
 
-    function realizeLoss(address adapter, bytes memory data) external {
+    /// @dev Returns incentiveShares, loss.
+    function realizeLoss(address adapter, bytes memory data) external returns (uint256, uint256) {
         require(isAdapter[adapter], ErrorsLib.NotAdapter());
 
         accrueInterest();
@@ -738,6 +739,7 @@ contract VaultV2 is IVaultV2 {
         }
 
         emit EventsLib.RealizeLoss(msg.sender, adapter, ids, loss, incentiveShares);
+        return (incentiveShares, loss);
     }
 
     /* ERC20 FUNCTIONS */
