f629174f0265a6a89a30aeb2563778246761b60c
Quentin Garchery <garchery.quentin@gmail.com>
2025-08-03 12:02:58 +0200

    docs: rounding errors are realizable

--- src/adapters/MorphoVaultV1Adapter.sol
+++ src/adapters/MorphoVaultV1Adapter.sol
@@ -11,11 +11,12 @@ import {MathLib} from "../libraries/MathLib.sol";
 
 /// @dev Designed, developed and audited for Morpho Vaults v1 (v1.0 and v1.1) (also known as MetaMorpho). Integration
 /// with other vaults must be carefully assessed from a security standpoint.
-/// @dev Morpho Vaults v1.1 do not realize bad debt, so Morpho Vaults v2 supplying in them will not realize the
-/// corresponding bad debt.
 /// @dev This adapter must be used with Morpho Vaults v1 that are protected against inflation attacks with an initial
 /// deposit. See https://docs.openzeppelin.com/contracts/5.x/erc4626#inflation-attack.
 /// @dev Must not be used with a Morpho Vault v1 which has a market with an Irm that can re-enter the parent vault.
+/// @dev Morpho Vaults v1.1 do not realize bad debt, so Morpho Vaults v2 supplying in them will not realize the
+/// corresponding bad debt.
+/// @dev Losses that correspond to rounding errors are realizable.
 /// @dev Shares of the Morpho Vault v1 cannot be skimmed (unlike any other token).
 contract MorphoVaultV1Adapter is IMorphoVaultV1Adapter {
     using MathLib for uint256;
