3d7428285d19a28f07629b7592dd908092676989
Adrien Husson <adhusson@gmail.com>
2025-08-28 18:38:39 +0200

    Update src/VaultV2.sol
    Co-authored-by: MathisGD <74971347+MathisGD@users.noreply.github.com>
    Signed-off-by: Adrien Husson <adhusson@gmail.com>

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -398,7 +398,7 @@ contract VaultV2 is IVaultV2 {
         emit EventsLib.RemoveAdapter(account);
     }
 
-    /// @dev Be particularly careful as this may irreversibly disable submit for a selector.
+    /// @dev This function require great caution because it can irreversibly disable submit for a selector.
     /// @dev Existing pending operations submitted before increasing a timelock can still be executed at the initial executableAt.
     /// previous timelock duration.
     function increaseTimelock(bytes4 selector, uint256 newDuration) external {
