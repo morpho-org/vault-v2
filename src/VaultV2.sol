4b1813ca0a6dce5639b1bba86ad395b17c53afd9
MathisGD <74971347+MathisGD@users.noreply.github.com>
2025-08-28 13:04:22 +0200

    Apply suggestions from code review
    Co-authored-by: Adrien Husson <adhusson@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -34,8 +34,8 @@ import {ISharesGate, IReceiveAssetsGate, ISendAssetsGate} from "./interfaces/IGa
 /// share price but not faster than the maxRate, and the interest are accrued only once per transaction. In order to
 /// protect against inflation attacks, the vault might need to be seeded with an initial deposit. See
 /// https://docs.openzeppelin.com/contracts/5.x/erc4626#inflation-attack
-/// @dev Donations and forceDeallocate penalties can be in part stolen by opportunistic depositors if they make the rate
-/// increase by a large factor. Setting a low maxRate prevents that by making the donation/penalty distributed over a
+/// @dev If they make the rate increase by a large factor, donations and forceDeallocate penalties can be in part stolen
+/// by opportunistic depositors. Setting a low maxRate prevents that by making the donation/penalty distributed over a
 /// long period.
 ///
 /// CAPS
