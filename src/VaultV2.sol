a69a1e9ab42d665f7d4ae32ce5584b2665a442ea
Quentin Garchery <garchery.quentin@gmail.com>
2025-09-23 15:58:19 +0200

    Update src/VaultV2.sol
    Co-authored-by: Adrien Husson <adhusson@gmail.com>
    Signed-off-by: Quentin Garchery <garchery.quentin@gmail.com>

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -127,7 +127,7 @@ import {IReceiveSharesGate, ISendSharesGate, IReceiveAssetsGate, ISendAssetsGate
 /// TIMELOCKS
 /// @dev The timelock duration of decreaseTimelock is the timelock duration of the function whose timelock is being
 /// decreased (e.g. the timelock of decreaseTimelock(addAdapter, ...) is timelock[addAdapter]).
-/// @dev It is still possible to submit change of the timelock duration of decreaseTimelock, but it won't have any
+/// @dev It is still possible to submit a change of the timelock duration of decreaseTimelock, but it won't have any
 /// effect and trying to execute this change will revert.
 /// @dev Multiple clashing data can be pending, for example increaseCap and decreaseCap, which can make so accepted
 /// timelocked data can potentially be changed shortly afterwards.
