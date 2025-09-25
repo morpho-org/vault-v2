7915f968e9377e36369d45a4a626ff892a83dc05
Quentin Garchery <garchery.quentin@gmail.com>
2025-09-25 06:55:48 +0200

    Update src/VaultV2.sol
    Co-authored-by: MathisGD <74971347+MathisGD@users.noreply.github.com>
    Signed-off-by: Quentin Garchery <garchery.quentin@gmail.com>

--- src/VaultV2.sol
+++ src/VaultV2.sol
@@ -127,8 +127,8 @@ import {IReceiveSharesGate, ISendSharesGate, IReceiveAssetsGate, ISendAssetsGate
 /// TIMELOCKS
 /// @dev The timelock duration of decreaseTimelock is the timelock duration of the function whose timelock is being
 /// decreased (e.g. the timelock of decreaseTimelock(addAdapter, ...) is timelock[addAdapter]).
-/// @dev It is still possible to submit a change of the timelock duration of decreaseTimelock, but it won't have any
-/// effect and trying to execute this change will revert.
+/// @dev It is still possible to submit changes of the timelock duration of decreaseTimelock, but it won't have any
+/// effect (and trying to execute this change will revert).
 /// @dev Multiple clashing data can be pending, for example increaseCap and decreaseCap, which can make so accepted
 /// timelocked data can potentially be changed shortly afterwards.
 /// @dev If a function is abdicated, it cannot be called no matter its timelock and what executableAt[data] contains.
