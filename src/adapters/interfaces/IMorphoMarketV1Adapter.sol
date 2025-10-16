46ff0f6a3886749292785100e653dc0b9bb260f9
Quentin Garchery <garchery.quentin@gmail.com>
2025-10-16 12:09:12 +0200

    refactor: new interface that returns a struct

--- src/adapters/interfaces/IMorphoMarketV1Adapter.sol
+++ src/adapters/interfaces/IMorphoMarketV1Adapter.sol
@@ -31,3 +31,30 @@ interface IMorphoMarketV1Adapter is IAdapter {
     function setSkimRecipient(address newSkimRecipient) external;
     function skim(address token) external;
 }
+
+interface IMorphoMarketV1AdapterReturnsStruct is IAdapter {
+    /* EVENTS */
+
+    event SetSkimRecipient(address indexed newSkimRecipient);
+    event Skim(address indexed token, uint256 assets);
+
+    /* ERRORS */
+
+    error LoanAssetMismatch();
+    error NotAuthorized();
+
+    /* FUNCTIONS */
+
+    function factory() external view returns (address);
+    function parentVault() external view returns (address);
+    function asset() external view returns (address);
+    function morpho() external view returns (address);
+    function adapterId() external view returns (bytes32);
+    function skimRecipient() external view returns (address);
+    function marketParamsList(uint256 index) external view returns (MarketParams memory);
+    function marketParamsListLength() external view returns (uint256);
+    function allocation(MarketParams memory marketParams) external view returns (uint256);
+    function ids(MarketParams memory marketParams) external view returns (bytes32[] memory);
+    function setSkimRecipient(address newSkimRecipient) external;
+    function skim(address token) external;
+}
