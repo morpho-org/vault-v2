f3291cc785cd2ae06e1a21853990feb0d2ebdba5
MathisGD <mathis@morpho.xyz>
2025-12-01 18:51:42 +0100

    docs: burn shares

--- src/adapters/MorphoMarketV1Adapter.sol
+++ src/adapters/MorphoMarketV1Adapter.sol
@@ -26,6 +26,10 @@ import {
 /// @dev Markets get removed from the marketIds when the allocation is zero, but it doesn't mean that the adapter has
 /// zero shares on the market.
 /// @dev This adapter can only be used for markets with the adaptive curve irm.
+///
+/// BURN SHARES
+/// @dev Burning shares takes time, so reactive depositors might be able to exit before the share price reduction.
+/// @dev It is possible to burn the shares of a market whose IRM reverts.
 contract MorphoMarketV1Adapter is IMorphoMarketV1Adapter {
     using MarketParamsLib for MarketParams;
     using SharesMathLib for uint256;
