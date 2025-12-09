39351852a0a25f4e66d316cdb2dfca88ac7988dd
Quentin Garchery <garchery.quentin@gmail.com>
2025-12-09 09:40:18 +0100

    fix: remove factory verification

--- src/adapters/MorphoVaultV1Adapter.sol
+++ src/adapters/MorphoVaultV1Adapter.sol
@@ -36,7 +36,7 @@ contract MorphoVaultV1Adapter is IMorphoVaultV1Adapter {
     /* FUNCTIONS */
 
     constructor(address _parentVault, address _morphoVaultV1) {
-        factory = address(1);
+        factory = msg.sender;
         parentVault = _parentVault;
         morphoVaultV1 = _morphoVaultV1;
         adapterId = keccak256(abi.encode("this", address(this)));
