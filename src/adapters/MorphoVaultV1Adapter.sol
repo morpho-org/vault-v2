fa17b7a96daf044e738b55df61574570f8915d99
Quentin Garchery <garchery.quentin@gmail.com>
2025-12-10 16:19:59 +0100

    temp: show counter example

--- src/adapters/MorphoVaultV1Adapter.sol
+++ src/adapters/MorphoVaultV1Adapter.sol
@@ -36,7 +36,7 @@ contract MorphoVaultV1Adapter is IMorphoVaultV1Adapter {
     /* FUNCTIONS */
 
     constructor(address _parentVault, address _morphoVaultV1) {
-        factory = msg.sender;
+        factory = address(1);
         parentVault = _parentVault;
         morphoVaultV1 = _morphoVaultV1;
         adapterId = keccak256(abi.encode("this", address(this)));
