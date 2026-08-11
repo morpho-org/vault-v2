ef20d753ad27d84d207413d02d18f9a4751a3589
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-08-11 16:11:17 +0200

    Update BluePublicAllocator.sol
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/periphery/blue-public-allocator/BluePublicAllocator.sol
+++ src/periphery/blue-public-allocator/BluePublicAllocator.sol
@@ -20,8 +20,8 @@ import {MarketParams} from "../../../lib/morpho-blue/src/interfaces/IMorpho.sol"
 /// it requires capital).
 /// @dev reallocate and allocateFromIdle can be made to revert by anyone frontrunning them (not only allocators): an
 /// allocate reverts if the vault cap is filled first, a deallocate reverts if shares stop covering assets.
-/// @dev Reallocations through this contract can reduce the vault's "direct" liquidity by pull assets from idle and from
-/// the liquidity market.
+/// @dev Reallocations (notably through this contract) can reduce the vault's "direct" liquidity by pull assets from
+/// idle and from the liquidity market. It can also block deposit by reaching the liquidity market's caps.
 contract BluePublicAllocator is IBluePublicAllocator {
     /* STORAGE */
 
