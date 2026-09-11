583d40ffc11fa8225282ebce0cbcc6b9017cb095
Adrien Husson <adhusson@gmail.com>
2026-09-11 17:56:36 +0200

    min rate

--- src/adapters/MidnightAdapter.sol
+++ src/adapters/MidnightAdapter.sol
@@ -11,6 +11,7 @@ import {TakeAmountsLib} from "lib/midnight/src/periphery/libraries/TakeAmountsLi
 import {IERC20} from "../interfaces/IERC20.sol";
 import {SafeERC20Lib} from "../libraries/SafeERC20Lib.sol";
 import {MathLib} from "../libraries/MathLib.sol";
+import {WAD} from "../libraries/ConstantsLib.sol";
 import {IVaultV2} from "../interfaces/IVaultV2.sol";
 import {IMidnightAdapter, MaturityData, MarketData, IAdapter} from "./interfaces/IMidnightAdapter.sol";
 import {DurationsLib} from "./libraries/DurationsLib.sol";
@@ -53,6 +54,8 @@ contract MidnightAdapter is IMidnightAdapter {
 
     address public skimRecipient;
     bool public skipBufferCheck;
+    /// @dev Minimum net simple interest rate per second, WAD-scaled, enforced on maker and taker buys before maturity.
+    uint256 public minRate;
     mapping(address subRatifier => bool) public isSubRatifier;
 
     /* ACCOUNTING */
@@ -211,6 +214,12 @@ contract MidnightAdapter is IMidnightAdapter {
         timelocked();
         skipBufferCheck = newSkipBufferCheck;
         emit SetSkipBufferCheck(newSkipBufferCheck);
+    }
+
+    function setMinRate(uint256 newMinRate) external {
+        timelocked();
+        minRate = newMinRate;
+        emit SetMinRate(newMinRate);
     }
 
     function setSkimRecipient(address newSkimRecipient) external {
@@ -436,6 +445,7 @@ contract MidnightAdapter is IMidnightAdapter {
 
         if (timeToMaturity > 0) {
             uint256 interest = boughtNetCredit - paidAssets;
+            require(paidAssets == 0 || interest.mulDivDown(WAD, paidAssets) / timeToMaturity >= minRate, RateTooLow());
             uint120 growthIncrease = (interest / timeToMaturity).toUint120();
             totalAssets += (paidAssets + interest % timeToMaturity).toUint128();
             marketData.growth += growthIncrease;
