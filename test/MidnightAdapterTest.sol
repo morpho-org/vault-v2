802b0b64656d10435328dc517c92c9eb1a94fa17
Adrien Husson <adhusson@gmail.com>
2026-09-11 16:49:48 +0200

    fix id string

--- test/MidnightAdapterTest.sol
+++ test/MidnightAdapterTest.sol
@@ -866,7 +866,7 @@ contract MidnightAdapterTest is Test {
                 ids[i * 2 + 2],
                 keccak256(
                     abi.encode(
-                        "collateral",
+                        "collateralParams",
                         market.collateralParams[i].token,
                         market.collateralParams[i].oracle,
                         market.collateralParams[i].lltv,
@@ -2289,7 +2289,7 @@ contract MidnightAdapterTest is Test {
         idDatas[0] = abi.encode("this", address(adapter));
         idDatas[1] = abi.encode("collateralToken", storedCollaterals[0].token);
         idDatas[2] = abi.encode(
-            "collateral",
+            "collateralParams",
             storedCollaterals[0].token,
             storedCollaterals[0].oracle,
             storedCollaterals[0].lltv,
@@ -2297,7 +2297,7 @@ contract MidnightAdapterTest is Test {
         );
         idDatas[3] = abi.encode("collateralToken", storedCollaterals[1].token);
         idDatas[4] = abi.encode(
-            "collateral",
+            "collateralParams",
             storedCollaterals[1].token,
             storedCollaterals[1].oracle,
             storedCollaterals[1].lltv,
